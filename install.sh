79a47b3da5f340f73c29561f7bae76d34fc9253c
隔壁小胡 <79695586+gbxh@users.noreply.github.com>
2022-10-31 16:54:58 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,32 @@
+cd ~
+wget https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
+tar -xvjf ffmpeg-snapshot.tar.bz2
+sudo apt-get update && sudo apt-get -y install \
+  autoconf \
+  automake \
+  build-essential \
+  cmake \
+  git-core \
+  libass-dev \
+  libfreetype6-dev \
+  libgnutls28-dev \
+  libmp3lame-dev \
+  libsdl2-dev \
+  libtool \
+  libva-dev \
+  libvdpau-dev \
+  libvorbis-dev \
+  libxcb1-dev \
+  libxcb-shm0-dev \
+  libxcb-xfixes0-dev \
+  meson \
+  ninja-build \
+  pkg-config \
+  texinfo \
+  wget \
+  yasm \
+  zlib1g-dev \
+  libunistring-dev
+cd ~/ffmpeg
+make
+sudo make install
